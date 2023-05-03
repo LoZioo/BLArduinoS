88ccc09248225b03498284d2f95a66791a648659
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-05-03 18:26:50 +0200

    feat: updated script.

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
-clear && g++ main.cpp && ./a.out
+#!/bin/zsh
+
+while true; do
+	clear && g++ main.cpp && ./a.out
+
+	echo "\nHit a key to continue or ESC to exit..."
+	read -skrn 1 key
+
+	if [[ $key == $'\x1b' ]]; then
+		echo "ESC pressed, exiting..."
+		break
+	fi
+done
